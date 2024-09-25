8a80d0a8200c084451518eb0b455021f12f2f77e
Dayana Peña <dayana@correo.com>
2024-09-25 12:02:13 +0200

    Esto es un commit de prueba. Estoy escribiendo en Visual Studio

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <main class="container">
         <section>
@@ -12,6 +14,15 @@
                 Título
             </h1>
         </section>
+        <section>
+            <h2>
+                Subtítulo
+            </h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel libero id neque consectetur consequat. Donec aliquet urna vel metus scelerisque, vitae consectetur felis bibendum. Donec semper, dui vel dictum pellentesque, nisi mauris congue eros, ut mollis nunc nisi vel ipsum. Nulla facilisi.
+            </p>
+        </section>
     </main>
 </body>
+
 </html>
